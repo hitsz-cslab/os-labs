029d957c0dc2e4fa0c5cf5ff644756c0fa337eff
greenhandzpx <893522573@qq.com>
2023-09-21 00:41:30 +0800

    Deployed 5504fef with MkDocs version: 1.5.3

---
+++ js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?eb44b99491a8d8d5e43a5b33a6e7c902";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
